201253a34f48d063b1e9f14d4007d8fc952ceaa1
Sachintha Umayanga <128795701+SachinthaUmayanga@users.noreply.github.com>
2024-11-09 23:31:46 +0530

    Create cart repository and cart interface

---
+++ Book_Store/Repositories/ICartRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Book_Store.Repositories
+{
+    public interface ICartRepository
+    {
+        Task<bool> AddItem(int bookId, int qty);
+        Task<bool> RemoveItem(int bookId);
+        Task<IEnumerable<ShoppingCart>> GetUserCart();
+    }
+}
