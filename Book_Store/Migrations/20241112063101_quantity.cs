942527bce84d207c8e7a5c363174d1f90471a539
Sachintha Umayanga <128795701+SachinthaUmayanga@users.noreply.github.com>
2024-11-12 13:01:56 +0530

    Create my orders section

---
+++ Book_Store/Migrations/20241112063101_quantity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Book_Store.Migrations
+{
+    /// <inheritdoc />
+    public partial class quantity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quanity",
+                table: "OrderDetail",
+                newName: "Quantity");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quantity",
+                table: "OrderDetail",
+                newName: "Quanity");
+        }
+    }
+}
---
+++ Book_Store/Repositories/IUserOrderRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Book_Store.Repositories
+{
+    public interface IUserOrderRepository
+    {
+        Task<IEnumerable<Order>> UserOrders();
+    }
+}
