523ba435d05c23bb7a8c42b316776f87046bfd75
LAPTOP-63SV8TPF\LENOVO <nishedha.srilak@gmail.com>
2024-12-29 15:06:42 +0530

    added missing dto's

---
+++ Book_Store/Models/DTOs/DashboardBookDetailsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Book_Store.Models.DTOs
+{
+    public class DashboardBookDetailsDTO
+    {
+        public string Category { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ Book_Store/Models/DTOs/DashboardDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace Book_Store.Models.DTOs
+{
+    public class DashboardDTO
+    {
+        public List<DashboardOrderDetailsDTO>? OrderDetails { get; set; }
+        public List<DashboardBookDetailsDTO>? NewBooks { get; set; }
+        public List<DashboardBookDetailsDTO>? UsedBooks { get; set; }    
+    }
+}
