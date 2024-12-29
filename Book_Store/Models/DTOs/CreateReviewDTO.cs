229aabcd702b3ba0537ce30abe7999f348bf2c37
Shalin <nirvan.work@gmail.com>
2024-12-29 22:34:33 +0530

    fix issues in reviews part

---
+++ Book_Store/Models/DTOs/CreateReviewDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Store.Models.DTOs
+{
+    public class CreateReviewDTO
+    {
+        [Required]
+        public int BookId { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        [MaxLength(40)]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
---
+++ Book_Store/Models/Review.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Book_Store.Models
+{
+    [Table("Review")]
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual IdentityUser User { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        [ForeignKey("BookId")]
+        public virtual Book Book { get; set; }
+    }
+}
